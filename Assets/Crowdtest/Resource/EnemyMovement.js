595ff3f1b98c052201c155861777a1a6b658d49f
JSandmark <johansandmark@gmail.com>
2014-11-25 17:36:43 +0100

    Updated AI stats
    Small change, just some random variables for AI speed etc

--- Assets/Crowdtest/Resource/EnemyMovement.js
+++ Assets/Crowdtest/Resource/EnemyMovement.js
@@ -7,6 +7,10 @@ function Start () {
 
 	//Assign the NavMeshAgent at game start
 	navComponent = this.transform.GetComponent(NavMeshAgent);
+	
+	navComponent.speed = Random.Range(2.0, 4.0);
+	navComponent.acceleration = Random.Range(8.0, 9.0);
+	navComponent.angularSpeed = Random.Range(90.0, 140.0);
 
 }
 

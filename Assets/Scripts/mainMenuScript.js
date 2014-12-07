0d20f9e402e2107c0d11ed519ab14f1ed9d13476
Robin Siestrunck <sqems@orange.fr>
2014-12-07 20:04:52 +0100

    Added game main menu

---
+++ Assets/Scripts/mainMenuScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var mainMenuTexture : Texture;
+var mainMenuFont : Font;
+ 
+function OnGUI() {
+	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),mainMenuTexture);
+
+	var yPos = Screen.height /6;
+	var xPos = Screen.width / 2 - 130;
+	
+	GUI.skin.button.font = mainMenuFont;
+	GUI.skin.button.fontSize = 26;
+	
+	if (GUI.Button(new Rect(xPos, yPos , 250, 50),"Start game")) 
+	{
+		Application.LoadLevel("debuglevel_1_with_mesh_and_enemies");
+	}
+	if (GUI.Button(new Rect(xPos, yPos * 2, 250, 50),"Quit game")) 
+	{
+		Application.Quit();
+	}
+}

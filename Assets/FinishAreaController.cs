1af4a882d08144e9d5ced76bbee5d5f52cdcd1f5
JSandmark <johansandmark@gmail.com>
2014-12-11 21:39:12 +0100

    Added FinishLevel and some usefull resources
    Added scene: FinishLevel
    Gfx: Chopper, exit sign
    Added Standard Assets: Skybox
    Asset: Substances, quick and "good" looking textures that can be
    configures in unity. Please let me know if you need assistance how to
    use this. Bricks, concreate, wood and more.

---
+++ Assets/FinishAreaController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishAreaController : MonoBehaviour {
+
+	//When a collision is triggered
+
+//	void OnCollisionEnter(Collision collision) {
+	void OnTriggerEnter(Collider collision) {
+
+		//if it is the player
+		if (collision.gameObject.tag == "Player"){
+
+			//Load the correct level
+			Application.LoadLevel("new_game");
+
+			}
+	}
+		                                          
+
+
+}

f4d172a3aed741dc213657f2efe622c5bfb42269
Dmitrij Lioubartsev <dmitrijl@kth.se>
2014-12-14 19:44:15 +0100

    SceneLevel mostly done, added prefabs

---
+++ Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelScript : MonoBehaviour {
+
+	public string NextLevelName;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PlayerBody") {
+			Application.LoadLevel(NextLevelName);
+		}
+	}
+}
